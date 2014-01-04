8ee584b887c5bbac1bc7f1ccfff8f2ea3ff308bd
Brian Park <brokenazn@gmail.com>
2014-01-04 02:18:24 -0800

    Integrated Parse
    Now hosted @ http://digitaltable.parseapp.com

---
+++ public/js/index.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  
+ window.fbAsyncInit = function() {
+  FB.init({
+    appId      : 235511756631747,
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
+
+  FB.Event.subscribe('auth.authResponseChange', function(response) {
+     if (response.status === 'connected') {
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+       FB.login();
+    } else {
+      FB.login();
+    }
+  });
+  };
+
+  // Load the SDK asynchronously
+  (function(d){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+  }(document));
+
+  // Here we run a very simple test of the Graph API after login is successful. 
+  // This testAPI() function is only called in those cases. 
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Good to see you, ' + response.name + '.');
+    });
+  }
+    $("#fb-login").click(function(){FB.login()});
+
+
+
+});
+
